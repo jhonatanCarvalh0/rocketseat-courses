652ac614518165f7bc3117d7b9c03bbbd334f6f4
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2024-03-29 22:38:35 -0400

    Update server.js
    Criação de funcionalidades básicas do servidor
    
    uma função que recbe request e devolve response, e definição de porta para ser 'escutada', onde ocorrera a comunicação entre browser e server

--- nodejs/modulo-01/src/server.js
+++ nodejs/modulo-01/src/server.js
@@ -1,1 +1,10 @@
 import http from 'node:http';
+
+const server = http.createServer((request, response) => {
+  return response.end('Hello World');
+});
+
+server.listen(3333, () => {
+  console.log('Server started on port 3333');
+    console.log('http://localhost:3333');
+});
